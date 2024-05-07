db295cbf86855ec5b8d6626e573bc69bead05ddf
Simon Berner <sibebzh@gmail.com>
2024-05-07 20:12:42 +0200

    init: :tada: setup the project

---
+++ ParrotRefactoringKata/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParrotRefactoringKata
+//
+//  Created by simon.berner on 07.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Parrot Refacoring Kata")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParrotRefactoringKata/ParrotRefactoringKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParrotRefactoringKataApp.swift
+//  ParrotRefactoringKata
+//
+//  Created by simon.berner on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ParrotRefactoringKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
